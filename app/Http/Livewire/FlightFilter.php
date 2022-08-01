62a5bd545e41dcec940b79a265ffbda55be33259
shukeel <triggaillest@gmail.com>
2022-08-01 16:32:36 +0300

    modified

---
+++ app/Http/Livewire/FlightFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FlightFilter extends Component
+{
+    public function render()
+    {
+        return view('livewire.flight-filter');
+    }
+}
