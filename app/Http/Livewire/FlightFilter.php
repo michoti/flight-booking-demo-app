5300de815d10e96cbeeb3d21c94fbdcf07d58c8c
shukeel <triggaillest@gmail.com>
2022-08-01 19:28:57 +0300

    modified

--- app/Http/Livewire/FlightFilter.php
+++ app/Http/Livewire/FlightFilter.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\FlightDetail;
 use Livewire\Component;
 
 class FlightFilter extends Component
 {
+    public $flights;
+    public $name;
+    public $departing_date;
+    
+    public function mount()
+    {
+        $this->flights = FlightDetail::all();
+    }
+
+    public function searching()
+    {
+        // $flight = FlightDetail::query();
+
+        if(!empty($this->name))
+        {
+            $flight = FlightDetail::query()->where('flight_source', 'like', '%'. $this->name .'%');
+            $this->flights = $flight->get();
+            return $this->flights;
+        }
+    }
+
     public function render()
     {
-        return view('livewire.flight-filter');
+        return view('livewire.flight-filter', [
+            'flights' => $this->flights,
+        ])
+        ->layout('layouts.app');
     }
 }
--- resources/views/livewire/flight-filter.blade.php
+++ resources/views/livewire/flight-filter.blade.php
@@ -1,41 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <div>
-        <form>
-            <div class="mb-6">
-                <input type="email" id="email"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@flowbite.com" required="">
-            </div>
-            <div class="mb-6">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
-                    password</label>
-                <input type="password" id="password"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required="">
-            </div>
-            <div class="flex items-start mb-6">
-                <div class="flex items-center h-5">
-                    <input id="remember" type="checkbox" value=""
-                        class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                        required="">
-                </div>
-                <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
-                    me</label>
-            </div>
-            <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-        </form>
+<form wire:submit.prevent='searching'>
+    <div class="flex items-start mb-6">
+        <input type="search" wire:model='name' id="password"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <input type="date" wire:model='departing_date' id="password"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                     focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 
+                     text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+        </button>
     </div>
-</body>
 
-</html>
+</form>
+
+<div class="overflow-x-auto relative">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">source</th>
+                <th scope="col" class="py-3 px-6">destination</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($flights as $flight)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td class="py-4 px-6">{{ $flight->flight_source}}</td>
+                <td class="py-4 px-6">{{ $flight->flight_destination}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
