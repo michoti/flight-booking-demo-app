05b9ec63d6dc05ce90fc4a441f38bf86ba383b7f
shukeel <triggaillest@gmail.com>
2022-08-04 12:36:52 +0300

    modified

--- app/Http/Livewire/FlightFilter.php
+++ app/Http/Livewire/FlightFilter.php
@@ -8,26 +8,7 @@ use Livewire\Component;
 use Livewire\WithPagination;
 
 class FlightFilter extends Component
-{
-    public $flights;
-    public $airlines = [];
-    public $search = '';
-
-    public $source, $destination;
-    public $departing_date;
-
-    // public function mount($flights)
-    // {
-    //     $this->flights = $flights;
-    // }
-
-    public function searching()
-    {
-        
-    }
-    
-
-
+{  
     public function render()
     {
         return view('livewire.flight-filter')->layout('layouts.app');
