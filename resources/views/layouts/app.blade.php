00d657eb1aa56d6eb277de76572ef151083b426e
shukeel <triggaillest@gmail.com>
2022-08-04 20:26:55 +0300

    modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{ asset('main.css')}}" rel="stylesheet" />
     {{-- @vite('resources/css/app.css') --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
         integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
@@ -16,7 +17,7 @@
 
 <body>
 
-    <div class="font-sans text-gray-900 antialiased">
+    <div>
         {{ $slot }}
     </div>
 
--- resources/views/livewire/flight-filter.blade.php
+++ resources/views/livewire/flight-filter.blade.php
@@ -1,23 +1,25 @@
 <div>
-    <div>
+    <div class="side-bar">
         <label><input type="checkbox" wire:model='airlines' value="1"> Airline</label>
         <label><input type="checkbox" wire:model='airlines' value="2"> Airline 2</label>
     </div>
-    <div>
-        <label>Departing date <input wire:model='departing_date' type="date"></label>
-        <label>Returning date <input wire:model='returning_date' type="date"></label>
-    </div>
-    @foreach ($flights as $flight)
-    <div wire:click='displayContent({{ $flight->id }})'>
-        <p>{{ $flight->flight_detail->departure_time}} - {{ $flight->flight_detail->arrival_time}}</p>
-        <p>{{ $flight->flight_detail->flight_source}} to {{ $flight->flight_detail->flight_destination}}</p>
+    <div class="main-content">
         <div>
-            <i class="fa fa-plane" aria-hidden="true"></i> {{ $flight->airline->name}}
+            <label>Departing date <input wire:model='departing_date' type="date"></label>
+            <label>Returning date <input wire:model='returning_date' type="date"></label>
         </div>
-    </div>
-    @endforeach
+        @foreach ($flights as $flight)
+        <div wire:click='displayContent({{ $flight->id }})'>
+            <p>{{ $flight->flight_detail->departure_time}} - {{ $flight->flight_detail->arrival_time}}</p>
+            <p>{{ $flight->flight_detail->flight_source}} to {{ $flight->flight_detail->flight_destination}}</p>
+            <div>
+                <i class="fa fa-plane" aria-hidden="true"></i> {{ $flight->airline->name}}
+            </div>
+        </div>
+        @endforeach
 
-    <div id="content">
+        <div id="content">
 
+        </div>
     </div>
 </div>
